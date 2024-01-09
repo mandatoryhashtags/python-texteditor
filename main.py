f3f5238bb62bfc6059597f1dc1ed6309ad56e8d3
Curtis Schrum <cschrum@galco.com>
2024-01-09 16:20:29 -0600

    Changed format to match pep8

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
+
 def open_file(window, text_edit):
-    filepath = askopenfilename(filetypes=[("Text Files", "*.txt"), ("Python Files", "*.py")])
-    
+    filepath = askopenfilename(
+        filetypes=[("Text Files", "*.txt"), ("Python Files", "*.py")])
+
     if not filepath:
         return
-    
+
     text_edit.delete(1.0, tk.END)
     with open(filepath, "r") as f:
         content = f.read()
@@ -14,12 +16,13 @@ def open_file(window, text_edit):
 
     window.title(f"Text by MHT: {filepath}")
 
+
 def save_file(window, text_edit):
-    filepath = asksaveasfilename(filetypes=[("Text Files", "*.txt"), ("Python Files", "*.py")])
+    filepath = asksaveasfilename(
+        filetypes=[("Text Files", "*.txt"), ("Python Files", "*.py")])
 
     if not filepath:
         return
-
 
     with open(filepath, "w") as f:
         content = text_edit.get(1.0, tk.END)
@@ -27,17 +30,17 @@ def save_file(window, text_edit):
 
     window.title(f"Text by MHT: {filepath}")
 
+
 def main():
     # Window Creation
     window = tk.Tk()
     window.title("Text by MHT")
-    window.rowconfigure(0,minsize=400)
-    window.columnconfigure(0,minsize=100)
-    window.columnconfigure(1,minsize=500)
-
+    window.rowconfigure(0, minsize=400)
+    window.columnconfigure(0, minsize=100)
+    window.columnconfigure(1, minsize=500)
 
     # Text Editor
-    text_edit = tk.Text(window,font="Helvetica 16")
+    text_edit = tk.Text(window, font="Helvetica 16")
     text_edit.grid(row=0, column=1)
 
     # Framing
@@ -46,10 +49,10 @@ def main():
 
     # Buttons
     save_button = tk.Button(
-        frame, text="Save", font="Helvetica 12", justify="center", padx=5, pady=5, 
+        frame, text="Save", font="Helvetica 12", justify="center", padx=5, pady=5,
         command=lambda: save_file(window, text_edit))
     open_button = tk.Button(
-        frame, text="Open", font="Helvetica 12", justify="center", padx=5, pady=5, 
+        frame, text="Open", font="Helvetica 12", justify="center", padx=5, pady=5,
         command=lambda: open_file(window, text_edit))
 
     save_button.grid(row=0, column=0, sticky="ew")
@@ -62,5 +65,6 @@ def main():
 
     # Running Loop
     window.mainloop()
+
 
 main()
